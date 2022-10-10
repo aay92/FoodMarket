0a6ef6313cb9809c33d802f9bcc53639c2665846
Aleksey <aay92@yandex.ru>
2022-10-10 11:57:52 +0400

    Initial Commit

---
+++ FoodMarket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodMarket
+//
+//  Created by Aleksey Alyonin on 10.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
